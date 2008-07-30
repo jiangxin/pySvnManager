71325fc093281288a35f7e33d95e43b4cca06c86
jiangx <jiangx@39c8d0d3-2d71-4a2c-8909-36513c942afe>
2008-07-30 01:59:56 +0000

    See #6: reset RCSINIT environment, or it may make something strange.
    git-svn-id: https://pysvnmanager.svn.sourceforge.net/svnroot/pysvnmanager/trunk@28 39c8d0d3-2d71-4a2c-8909-36513c942afe

--- pysvnmanager/model/rcsbackup.py
+++ pysvnmanager/model/rcsbackup.py
@@ -11,6 +11,11 @@ import sys
             #will be delete after initialize. we need reload it.
 #sys.setdefaultencoding('utf-8')
 
+CMD_CI="RCSINIT= ci"
+CMD_CO="RCSINIT= co"
+CMD_RCS="RCSINIT= rcs"
+
+
 def is_rcs_exist(wcfile):
     wcpath = os.path.dirname(os.path.abspath(wcfile))
     if os.path.isdir(wcpath+'/RCS'):
@@ -49,12 +54,17 @@ def backup(wcfile, comment='', user=''):
     
     cmd = []
     if not is_rcs_exist(wcfile):
-        cmd.append("""ci -i -q -t-"%(msg)s" -w"%(user)s" %(file)s 2>&1""" % \
-                {"file":wcfile, "msg":comment, "user":user})
-        cmd.append('rcs -U -q %s' % wcfile)
+        # -l : lock mode, make wcfile writable
+        cmd.append('%(cmd)s -i -q -l -t-"%(msg)s" -w"%(user)s" %(file)s 2>&1' % \
+                {'cmd':CMD_CI, "file":wcfile, "msg":comment, "user":user})
+        # -U : set locking to no-strict.
+        cmd.append('%(cmd)s -U -q %(file)s' % {'cmd':CMD_RCS, "file":wcfile})
     else:
-        cmd.append("""ci -q -m"%(msg)s" -w"%(user)s" %(file)s 2>&1""" % \
-                {"file":wcfile, "msg":comment, "user":user})
+        # Warning: w/o -l or -u option, wcfile will be removed after checkin.
+        # -l makes wcfile writable;
+        # -u : wcfile is not writable unless rcsfile is set to no-strict locking.
+        cmd.append('%(cmd)s -q -l -m"%(msg)s" -w"%(user)s" %(file)s 2>&1' % \
+                {'cmd':CMD_CI, "file":wcfile, "msg":comment, "user":user})
 
     for i in cmd:
         log.debug("Command: "+i)
@@ -71,13 +81,12 @@ def restore(wcfile, revision=""):
         return
     opts = ""
     if revision:
+        # -uRev : checkout without lock revision.
+        # unlock wcfile is readonly, unless no-strict mode is set.
         opts = "-u%s" % revision
 
-    cmd = "co %(opts)s -q -f %(file)s 2>&1" % {"opts":opts, "file":wcfile }
+    cmd = "%(cmd)s %(opts)s -q -f %(file)s 2>&1" % {'cmd':CMD_CO, "opts":opts, "file":wcfile }
     buff = os.popen(cmd).read().strip()
     if buff:
-        if 'co: warning: -l overridden by -u' in buff:
-            pass
-        else:
-            raise Exception, "Command: %s\nError Message: %s\n" % (cmd, buff)
+        raise Exception, "Command: %s\nError Message: %s\n" % (cmd, buff)
     
