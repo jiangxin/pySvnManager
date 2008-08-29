ca93a58adfb96acc8715d1af66cb2b9c2deba2ea
jiangx <jiangx@39c8d0d3-2d71-4a2c-8909-36513c942afe>
2008-08-29 07:25:05 +0000

    add copyright header
    git-svn-id: https://pysvnmanager.svn.sourceforge.net/svnroot/pysvnmanager/trunk@50 39c8d0d3-2d71-4a2c-8909-36513c942afe

--- pysvnmanager/tests/functional/test_login.py
+++ pysvnmanager/tests/functional/test_login.py
@@ -1,4 +1,20 @@
 ## -*- coding: utf-8 -*-
+#
+# Copyright (C) 2008 OpenSourceXpress Ltd. (http://www.ossxp.com)
+# Author: Jiang Xin
+# Contact: http://www.ossxp.com
+#          http://www.worldhello.net
+#          http://moinmo.in/JiangXin
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
 from pysvnmanager.tests import *
 from pylons import config
